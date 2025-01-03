e593d53916ac22a4911cfc021c9c605579554e57
Diego Gaviola <dgaviola@slingr.io>
2025-01-03 18:01:37 -0300

    Fixing webhooks

--- listeners/webhook.js
+++ listeners/webhook.js
@@ -14,12 +14,13 @@ listeners.defaultWebhookAIStudio = {
     },
     callback: function(event) {
         let headers = event.data.headers;
-        let webhookSecret = headers['AI-Studio-Webhook-Secret'];
+        let data = event.data.body;
+        let webhookSecret = headers['AI-Studio-Webhook-Secret'] || headers['ai-studio-webhook-secret'];
 
         if (pkg.aistudio.utils.verifyWebhookSecret(webhookSecret)) {
-            let eventType = event.data.type;
+            let eventType = data.type;
             if (eventType == 'taskReady') {
-                let taskId = event.data.taskId;
+                let taskId = data.taskId;
                 // If this is an event of type taskReady we will try to find a callback for it
                 let callback = sys.storage.get(`aistudio_task_callback_${taskId}`);
                 if (callback) {
@@ -28,21 +29,21 @@ listeners.defaultWebhookAIStudio = {
                         let params = callback.slice(callback.indexOf('(') + 1, callback.indexOf(')'));
                         let body = callback.slice(callback.indexOf('{') + 1, callback.lastIndexOf('}'));
                         let callbackFunction = new Function(params, body);
-                        callbackFunction(taskId, event.data.response);
+                        callbackFunction(taskId, data.response);
                     } catch (e) {
                         sys.logs.error(`[aistudio] Error executing callback for task [${taskId}]`);
                     } finally {
                         sys.storage.remove(`aistudio_task_callback_${taskId}`);
-                        event.data.callbackExecuted = true;
+                        data.callbackExecuted = true;
                     }
                 } else {
-                    event.data.callbackExecuted = false;
+                    data.callbackExecuted = false;
                 }
                 // We will also put the response in the storage in case someone is waiting for it (only 10 minutes)
-                sys.storage.put(`aistudio_task_response_${taskId}`, event.data.response, {ttl: 1000 * 60 * 10});
+                sys.storage.put(`aistudio_task_response_${taskId}`, data.response, {ttl: 1000 * 60 * 10});
             }
             // Then, we send the event
-            sys.events.triggerEvent("aistudio:webhook", event.data);
+            sys.events.triggerEvent("aistudio:webhook", data);
         } else {
             sys.logs.warn('[aistudio] Invalid webhook secret');
         }
